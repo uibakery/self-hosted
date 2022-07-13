06424a526f1146fa1ca547188d1f475999a2167b
Your Name <staydol@gmail.com>
2022-07-13 09:31:50 +0300

    Install docker compose via curl

--- install.sh
+++ install.sh
@@ -152,7 +152,7 @@ if [[ "$NEED_INSTALL_DOCKER_COMPOSE" == "YES" ]]; then
    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
-echo "Dounload setup files ----------------------------------"
+echo "Download setup files ----------------------------------"
 
 [ -d ./ui-bakery-on-premise ] || mkdir ui-bakery-on-premise
 cd ui-bakery-on-premise
