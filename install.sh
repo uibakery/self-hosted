13d6f2a03c789c7a98104328c610fce7b8a7a504
Your Name <staydol@gmail.com>
2022-07-13 17:36:16 +0300

    Install docker compose via curl

--- install.sh
+++ install.sh
@@ -148,7 +148,7 @@ fi
 if [[ "$NEED_INSTALL_DOCKER_COMPOSE" == "YES" ]]; then
    echo "----------------------------------------------------"
    echo "Installing Docker-compose  ....."
-   sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+   sudo curl -L "https://github.com/docker/compose/releases/download/${MIN_VERSION_DOCKER_COMPOSE}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
    sudo chmod +x /usr/local/bin/docker-compose
 fi
 
