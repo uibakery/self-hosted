a2fb9a8386479159c3ceb310c920c615355c769a
nixa <4dmitr@gmail.com>
2023-05-29 18:41:45 +0300

    fix: remove docker version checker

--- install.sh
+++ install.sh
@@ -14,52 +14,6 @@ curl --connect-timeout 10 --max-time 20 -s -XPOST -H "Content-type: application/
 MIN_VERSION_DOCKER="20.10.11"
 MIN_VERSION_DOCKER_COMPOSE="1.29.2"
 
-function check_version {
-
- local Q_INSTALL_DOCKER="NO"
- local IFS=""
-
- if [[ "$1" == "DOCKER" ]]; then
-   IFS='.' read -ra min_version <<< "$MIN_VERSION_DOCKER"
- else
-   IFS='.' read -ra min_version <<< "$MIN_VERSION_DOCKER_COMPOSE"
- fi
-
- IFS=' ' read -ra version_array <<< "$2"
- IFS='.' read -ra subsubversion_array <<< "${version_array[2]}"
-
- local c_version="${subsubversion_array[0]//[[:space:]]/}"
- if (( $c_version < "${min_version[0]}" )); then
-   Q_INSTALL_DOCKER="YES"
- elif (( $c_version > "${min_version[0]}" )); then
-   Q_INSTALL_DOCKER="NO"
- else
-    local c_subversion="${subsubversion_array[1]//[[:space:]]/}"
-    if (( $c_subversion < "${min_version[1]}" )); then
-       Q_INSTALL_DOCKER="YES"
-    elif (( $c_subversion > "${min_version[1]}" ));  then
-       Q_INSTALL_DOCKER="NO"
-    else
-       IFS=',' read -ra c_subsubversion_array <<< "${subsubversion_array[2]}"
-       local c_subsubversion="${c_subsubversion_array[0]//[[:space:]]/}"
-       if (( $c_subsubversion < "${min_version[2]}" ));  then
-          Q_INSTALL_DOCKER="YES"
-       else
-          Q_INSTALL_DOCKER="NO"
-       fi
-    fi
- fi
- local Q_NEED_INSTALL_DOCKER=0
- if [[ "$Q_INSTALL_DOCKER" == "YES" ]]; then
-     echo "The minimum required version should be ${min_version[0]}.${min_version[1]}.${min_version[2]}"
-     echo -e "\033[0m\033[0m\033[31m You need to manually upgrade the component to at least the minimum required version. Installation will be aborted."
-     echo -e "\033[m"
-     Q_NEED_INSTALL_DOCKER=1
- fi
-
-  return $Q_NEED_INSTALL_DOCKER
-}
-
 OS_ID=""
 if [ -r /etc/os-release ]; then
  OS_ID="$(. /etc/os-release && echo "$ID")"
@@ -75,13 +29,6 @@ if [ -n "$I" ]; then
    if [[ "$OS_ID" == "$OS_AUTO_INSTALL" ]]; then
        J=`docker -v`
        echo "$J"
-       check_version "DOCKER" "$J"
-       f_result=$?
-       if (( $f_result == 0 )); then
-          NEED_INSTALL_DOCKER="NO"
-       else
-          NEXT_INSTALL_OPERATION="EXIT"
-       fi
    else
       echo "Make sure the installed version of docker is ${MIN_VERSION_DOCKER} or higher. Continue? Y/n (Default - Y)"
       while read docker_version_y_n; do
@@ -112,13 +59,6 @@ if [ -n "$I" ]; then
    if [[ "$OS_ID" == "$OS_AUTO_INSTALL" ]]; then
        J=`docker-compose -v`
        echo "$J"
-       check_version "DOCKER_COMPOSE" "$J"
-       f_result=$?
-       if (( $f_result == 0 )); then
-         NEED_INSTALL_DOCKER_COMPOSE="NO"
-       else
-         NEXT_INSTALL_OPERATION="EXIT"
-       fi
     else
       echo "Make sure the installed version of docker-compose is ${MIN_VERSION_DOCKER_COMPOSE} or higher. Continue? Y/n (Default - Y)"
       while read docker_compose_version_y_n; do
